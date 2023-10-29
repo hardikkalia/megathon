a8dc3e318373ed9d72002864d1741968a15934f2
Harshit16468 <127506744+Harshit16468@users.noreply.github.com>
2023-10-29 13:29:53 +0530

    Update threads.py

--- threads.py
+++ threads.py
@@ -1,11 +1,13 @@
 from threads_interface import ThreadsInterface
 
 scraper = ThreadsInterface()
-
-user_id = scraper.retrieve_user_id('brfootball')
+id=''
+user_id = scraper.retrieve_user_id(id)
 user_data = scraper.retrieve_user_threads(user_id)
 threads=user_data['data']['mediaData']['threads']
 captions=[]
 for thread in threads:
     captions.append(thread['thread_items'][0]['post']['caption']['text'])
-print(captions)
+with open('threads_data.txt','+wt') as f:
+    for caption in captions:
+        f.write(caption+"\n")
