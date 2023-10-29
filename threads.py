45779bc64d8959e768ba569cdf7a2f89aad4087d
Hardik Kalia <hardikkalia@Hardiks-MacBook-Air.local>
2023-10-29 13:18:32 +0530

    threads

--- threads.py
+++ threads.py
@@ -1,8 +1,8 @@
 from threads_interface import ThreadsInterface
 
 scraper = ThreadsInterface()
-
-user_id = scraper.retrieve_user_id('brfootball')
+id=''
+user_id = scraper.retrieve_user_id(id)
 user_data = scraper.retrieve_user_threads(user_id)
 threads=user_data['data']['mediaData']['threads']
 captions=[]
@@ -10,4 +10,4 @@ for thread in threads:
     captions.append(thread['thread_items'][0]['post']['caption']['text'])
 with open('threads_data.txt','+wt') as f:
     for caption in captions:
-        f.write(caption)
+        f.write(caption+"\n")
